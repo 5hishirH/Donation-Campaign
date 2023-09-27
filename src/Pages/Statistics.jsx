dc737c4118d435cdd9fae1ede3560d5a85c7de06
Shishir Halder <s.halder0108@gmail.com>
2023-09-27 20:09:10 +0600

    feat: add basic pie chart

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import { PieChart, Pie } from 'recharts';
+
 const Statistics = () => {
+    const donateData = JSON.parse(localStorage.getItem('linkon'));
+    let donated = donateData.length;
+    console.log(donated);
+    let donatedPercent = donated*100/12;
+    let notDonated = 100 - donatedPercent;
+    console.log(donatedPercent, notDonated);
+
+    const data = [
+        { name: 'donated', value: donatedPercent },
+        { name: 'notDonated', value: notDonated }
+    ];
+
+    const COLORS = ['#0088FE', '#00C49F'];
     return (
         <div>
-            Statistics Route
+            <PieChart width={700} height={700}>
+            <Pie data={data} dataKey="value" outerRadius={250} fill="green" />
+        </PieChart>
         </div>
     )
 }
